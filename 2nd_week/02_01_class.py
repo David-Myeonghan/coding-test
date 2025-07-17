45fb1e935bbd4ec9d1bd864c297892c559378ed4
davidRyu <david.ryu@imagoworks.ai>
2025-07-18 00:19:27 +0900

    init class

---
+++ 2nd_week/02_01_class.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def talk(self):
+        print('%s is %d years old' % (self.name, self.age))
+
+person1 = Person('John', 20)
+print(person1)
+person1.talk()
