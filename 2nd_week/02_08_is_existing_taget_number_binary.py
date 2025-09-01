a152677782069ad199a5f06c972191ac933ec2ae
David-Myeonghan <myeonghan12@gmail.com>
2025-09-02 00:02:53 +0900

    find_count

--- 2nd_week/02_08_is_existing_taget_number_binary.py
+++ 2nd_week/02_08_is_existing_taget_number_binary.py
@@ -6,9 +6,12 @@ def is_existing_target_number_binary(target, array):
     current_min_index = 0
     current_max_index = len(array) - 1
     current_guess_index = (current_min_index + current_max_index) // 2
+    find_count = 0
 
     while current_min_index <= current_max_index:
+        find_count += 1
         if array[current_guess_index] == target:
+            print(find_count)
             return True
         elif array[current_guess_index] < target: # UP
             current_min_index = current_guess_index + 1
@@ -21,3 +24,20 @@ def is_existing_target_number_binary(target, array):
 
 result = is_existing_target_number_binary(finding_target, finding_numbers)
 print(result)
+
+# Array length: N
+# 1st: 1 ... N
+# 2nd: 1 ... N/2
+# 3rd: 1 ... N/4
+# kth: 1 ... N/2^k
+#
+# k번 탐색 -> N/2^k 개가 남는다
+#
+# N/2^k -> 1이 되려면, N = 2^k
+# log_2(N) = K
+#
+# k 번 탐색시 원하는 1개 원소를 찾을 수 있다.
+# O(log_2(N))
+# ==> O(log(N))
+#
+# log(N) --> 연산량이 반씩 줄어들면 대강 로그N 이다!
