ae339a94835a90b283af2b9b8f7e680ac065c289
David-Myeonghan <myeonghan12@gmail.com>
2025-11-17 22:53:03 +0900

    Stack problem - using loop

--- 3rd_week/03_07_get_receiver_top_orders.py
+++ 3rd_week/03_07_get_receiver_top_orders.py
@@ -1,9 +1,35 @@
 top_heights = [6, 9, 5, 7, 4]
-top_heights.append(1)
-print(top_heights)
+
+# 반복문 이용
+# 가장 오른쪽 탑부터 왼쪽으로 쏘면서 반복.
+# 1 2 3 4 5 || 5번째 탑부터 --> 탑5가 탑4보다 같거나 큰지 --> 탑5가 탑3보다 같거나 큰지 --> ... 탑5가 ... 탑1보다 같거나 큰지
+# v v v v
+# 1 2 3 4 || 4번째 탑부터
+# v v v
+# 1 2 3  || 3번째 탑부터
+# v v
+# 1 2  || 2번째 탑부터
+# v
+# --> 인덱스 4,3,2,1 순으로 뽑아야 한다. (맨 오른쪽 탑부터 비교)
+
+# 5번째 탑부터 --> 탑5가 탑4보다 같거나 큰지 --> 탑5가 탑3보다 같거나 큰지 --> ... 탑5가 ... 탑1보다 같거나 큰지
+# --> 인덱스 3,2,1 0 순으로 뽑아야 한다.
+
+# 아래와 같은 반복문을 이용해야 한다.
+# for i in range(4, 0, -1):
+#     for j in range(i - 1, -1, -1):
+#         print(i, j)
 
 def get_receiver_top_orders(heights):
-    return
+    result = [0] * len(heights)
+
+    for i in range(len(heights) - 1, 0, -1):
+        for j in range(i - 1, -1, -1):
+            if heights[i] < heights[j]:
+                result[i] = j + 1
+                break
+
+    return result
 
 
 print(get_receiver_top_orders(top_heights))  # [0, 0, 2, 2, 4] 가 반환되어야 한다!
