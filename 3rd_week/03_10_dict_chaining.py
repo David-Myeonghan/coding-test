718e21638bb1722659169dd7840225814c9eaf41
David-Myeonghan <myeonghan12@gmail.com>
2025-11-24 23:57:53 +0900

    linked_Dict

--- 3rd_week/03_10_dict_chaining.py
+++ 3rd_week/03_10_dict_chaining.py
@@ -13,27 +13,29 @@ class LinkedTuple:
             if k == key:
                 return v
 
-linked_tuple = LinkedTuple()
+# linked_tuple = LinkedTuple()
+#
+# linked_tuple.add('a', 1)
+# linked_tuple.add('b', 2)
+#
+# print(linked_tuple.items)
+# print(linked_tuple.get('b'))
 
-linked_tuple.add('a', 1)
-linked_tuple.add('b', 2)
-
-print(linked_tuple.items)
-print(linked_tuple.get('b'))
-
-class Dict:
+class LinkedDict:
     def __init__(self):
-        self.items = [None] * 8
+        self.items = []
+        for i in range(8):
+            self.items.append(LinkedTuple())
 
     def put(self, key, value):
         index = hash(key) % len(self.items)
-        self.items[index] = value
+        self.items[index].add(key, value)
 
     def get(self, key):
         index = hash(key) % len(self.items)
-        return self.items[index]
+        return self.items[index].get(key)
 
-my_dict = Dict()
+my_dict = LinkedDict()
 
 my_dict.put('test', 3)
 print(my_dict.get('test'))
