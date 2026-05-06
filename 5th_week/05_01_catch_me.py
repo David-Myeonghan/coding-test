b46f98319b3ec63a4594b4825c4324c36a9cdd54
David-Myeonghan <myeonghan12@gmail.com>
2026-05-07 00:00:05 +0900

    sdd

---
+++ 5th_week/05_01_catch_me.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+c = 11
+b = 2
+
+
+def catch_me(cony_loc, brown_loc):
+    # 구현해보세요!
+    return
+
+
+print(catch_me(c, b))  # 5가 나와야 합니다!
+
+print("정답 = 3 / 현재 풀이 값 = ", catch_me(10,3))
+print("정답 = 8 / 현재 풀이 값 = ", catch_me(51,50))
+print("정답 = 28 / 현재 풀이 값 = ", catch_me(550,500))
